06a10355a0dd22ba148a329d05b9fc3b97c3b785
JCastillo721 <vladimircastillo14@yahoo.es>
2020-06-06 23:20:57 -0600

    Probando
    Probando

--- ProyectoInventarioWeb/App_Start/RouteConfig.cs
+++ ProyectoInventarioWeb/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace ProyectoInventarioWeb
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Acceso", action = "Login", id = UrlParameter.Optional }
             );
         }
     }
